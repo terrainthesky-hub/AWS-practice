4ae81cb9b4faecc17ba4fff7898b39839f4cd5da
Lesley Rich <lesley.t.rich@gmail.com>
2021-07-06 16:27:47 -0700

    passwords

--- app/connectivity.py
+++ app/connectivity.py
@@ -11,13 +11,9 @@ pw = os.getenv('PASSWORD')
 un = os.getenv('USER_NAME')
 url = os.getenv("URL")
 
-USER_NAME="terra"
-PASSWORD="AkuMa2tRon!"
-URL="database-2.c0oecrjjdo0a.us-east-1.rds.amazonaws.com"
-
 # Replace username, password, & blah.blah.blah
 # database_url = f'postgresql://{un}:{pw}@{url}/postgres'
-# database_url = 'postgresql://terra:AkuMa2tRon!@database-2.c0oecrjjdo0a.us-east-1.rds.amazonaws.com/postgres'
-database_url = 'postgresql://terra:AkuMa2tRon!@terradb.c0oecrjjdo0a.us-east-1.rds.amazonaws.com/postgres'
+# 
+
 engine = sqlalchemy.create_engine(database_url)
 connection = engine.connect()
